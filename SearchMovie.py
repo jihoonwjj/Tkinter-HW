26f16a06d82b3a272238f578e16873f2fb8c9553
jihoonwjj <wangjajihooon@gmail.com>
2024-11-04 18:20:24 +0900

    ang

--- SearchMovie.py
+++ SearchMovie.py
@@ -7,6 +7,7 @@ import pygame
 win = Tk()
 pygame.init()
 win.geometry('1540x1280')
+
 win.title("영화 검색")
 win.option_add("*Font", "궁서체")
 win.iconphoto(False, PhotoImage(file="images\재욱송.png"))
@@ -26,16 +27,18 @@ msf.place(x=0, y=0)
 msf.tkraise()
 
 # 오디오, 이미지
-sound = pygame.mixer.Sound("bgms\pedro-song.wav")
-intotheunknown = pygame.mixer.Sound("bgms\Idina Menzel, AURORA - Into the Unknown (From ＂Frozen 2＂).wav")
-pororo = pygame.mixer.Sound("bgms\[뽀로로 1기] 오프닝 송♪ 노는 게 제일 좋아♪.wav")
-jangiha = pygame.mixer.Sound("bgms\장기하와 얼굴들 - 풍문으로 들었소 (범죄와의 전쟁： 나쁜놈들 전성시대 OST).wav")
-duckply = pygame.mixer.Sound("bgms\체리필터 (cherryfilter) - 오리 날다 [가사⧸Lyrics].wav")
-seeyouagain = pygame.mixer.Sound("bgms\故폴 워커 추모곡 See You Again Wiz Khalifa (ft. Charlie Puth) 가사 번역 뮤직비디오 (분노의 질주 더 세븐 OST).wav")
-homealone = pygame.mixer.Sound("bgms\나홀로집에 Home Alone OST - Somewhere in My Memory [가사해석 번역].wav")
-jojoost = pygame.mixer.Sound("bgms\Giornos Theme, but only the best part.wav")
-jjangguost = pygame.mixer.Sound("bgms\짱구는 못말려 OST - 오프닝1.wav")
-aotost = pygame.mixer.Sound("bgms\진격의 거인 1기 op.wav")
+sound = pygame.mixer.Sound(r"bgms\pedro-song.wav")
+intotheunknown = pygame.mixer.Sound(r"bgms\Idina Menzel, AURORA - Into the Unknown (From Frozen 2 ).wav")
+pororo = pygame.mixer.Sound(r"bgms\[뽀로로 1기] 오프닝 송♪ 노는 게 제일 좋아♪.wav")
+jangiha = pygame.mixer.Sound(r"bgms\장기하와 얼굴들 - 풍문으로 들었소 가사 (Synced Lyrics).wav")
+duckply = pygame.mixer.Sound(r"bgms\체리필터 (cherryfilter) - 오리 날다 [가사 Lyrics].wav")
+seeyouagain = pygame.mixer.Sound(r"bgms\Wiz Khalifa - See You Again ft. Charlie Puth [Official Video] Furious 7 Soundtrack.wav")
+homealone = pygame.mixer.Sound(r"bgms\나홀로집에 Home Alone OST - Somewhere in My Memory [가사해석 번역].wav")
+jojoost = pygame.mixer.Sound("bgms\\Giornos Theme best part but perfect ending.wav")
+jjangguost = pygame.mixer.Sound("bgms\\짱구는 못말려 OST - 오프닝1.wav")
+niggacat = pygame.mixer.Sound("bgms\\meow-meow-n-gga.wav")
+aotost = pygame.mixer.Sound(r"bgms\진격의 거인 1기 op.wav")
+niggacatImage = ImageTk.PhotoImage(Image.open(r"images\black cat.jpg").resize((1000,800)))
 homealoneImage = Image.open("images\나홀로집에.jpeg")
 homealoneImage = homealoneImage.resize((400,400))
 homealoneImage = ImageTk.PhotoImage(homealoneImage)
@@ -72,6 +75,19 @@ sound.play(-1)
 global getCountry
 global getJangr
 
+def soundPlay(m): 
+    currentMusic.stop()
+    m.play(-1)
+
+def quitApp():
+    sound.stop()
+    sound.set_volume(100)
+    label = Label(win, image=niggacatImage)
+    label.place(x=300,y=0)
+    niggacat.play()
+    label.tkraise()
+    win.after(1205, exit)
+
 def change_volume(vol):
     volume = float(vol) / 100
     sound.set_volume(volume)
@@ -81,6 +97,9 @@ def removemsfframe():
     msf.destroy()
 
 def getEntity():
+    global currentMusic
+
+    for i in movieInfo.winfo_children(): i.destroy()
     movie = '0'
     selectMovie = list()
     selectMovie.clear()
@@ -89,12 +108,10 @@ def getEntity():
     country.delete(0, END)
     jangr.delete(0, END)
 
-    if getCountry == "한" or getCountry == "미" or getCountry == "일":
-        pass
+    if getCountry == "한" or getCountry == "미" or getCountry == "일": pass
     else: getCountry = '0'
 
-    if getJangr == "애니메이션" or getJangr == "액션" or getJangr == "코미디":
-        pass
+    if getJangr == "애니메이션" or getJangr == "액션" or getJangr == "코미디": pass
     else: getJangr = '0'
 
     try:
@@ -125,7 +142,32 @@ def getEntity():
     
     if movie[2] == "겨울왕국2":
         Label(movieInfo, image=frozenImage).grid()
-
+        currentMusic = intotheunknown
+    elif movie[2] == "분노의 질주":
+        Label(movieInfo, image=bunnyo).grid()
+        currentMusic = seeyouagain
+    elif movie[2] == "나 홀로 집에":
+        Label(movieInfo, image=homealoneImage).grid()
+        currentMusic = homealone
+    elif movie[2] == "범죄와의 전쟁":
+        Label(movieInfo, image=warwithcrime).grid()
+        currentMusic = jangiha
+    elif movie[2] == "뽀롱뽀롱 뽀로로":
+        Label(movieInfo, image=pororoImage)
+        currentMusic = pororo
+    elif movie[2] == "권순분 여사 납치사건":
+        Label(movieInfo, image=namunheui)
+        currentMusic = duckply
+    elif movie[2] == "죠죠의 기묘한 모험":
+        Label(movieInfo, image=jojoImage)
+        currentMusic = jojoost
+    elif movie[2] == "진격의 거인":
+        Label(movieInfo, image=aot)
+        currentMusic = aotost
+    elif movie[2] == "짱구는 못말려":
+        Label(movieInfo, image=jjanggu)
+        currentMusic = jjangguost
+    else: currentMusic = FALSE
 
 angle = 0
 
@@ -150,7 +192,7 @@ Label(msf, text="영화 검색", width=10, height=0, font=("궁서체", 70) ,bg=
 labelJaeuk = Label(msf, image=piJaeuk)
 labelJaeuk.place(x=480,y=200)
 Button(msf, text="Play", font=("궁서체", 20), bg="gray", bd=2, command=removemsfframe, padx=70, pady=40, cursor="circle").place(x=485, y=500)
-Button(msf, text="Exit", font=("궁서체", 20), bg="gray", bd=2, command=exit, padx=70, pady=40, cursor="circle").place(x=915, y=500)
+Button(msf, text="Exit", font=("궁서체", 20), bg="gray", bd=2, command=quitApp, padx=70, pady=40, cursor="circle").place(x=915, y=500)
 
 # 메인 프레임
 Label(f, text="나라이름입력(한,미,일)", bg="gray").grid(row=0,column=0, sticky="e")
@@ -163,8 +205,8 @@ jangr.grid(row=1,column=1, padx=50, pady=30, sticky="e")
 moviee.grid(row=2, column=1, padx=50, pady=30, sticky="e")
 musicn.grid(row=3, column=1, padx=50, pady=30, sticky="e")
 Label(f, text="장르검색(액션,애니메이션,코미디)", padx=30, pady=10, bg="gray").grid(row=1,column=0,sticky="w")
-Button(f, text="입력", width=3, height=1, padx=30, pady=10, command=getEntity, bg="gray").grid(row=1, column=2, sticky="e")
-Button(f, text="Play Song", width=9, height=2, bg="gray").grid(row=3, column=2, sticky="w")
+Button(f, text="입력", width=3, height=1, padx=30, pady=10, command=getEntity, bg="gray", cursor="circle").grid(row=1, column=2, sticky="e")
+Button(f, text="Play Song", width=9, height=2, bg="gray", cursor="circle", command=lambda: soundPlay(currentMusic)).grid(row=3, column=2, sticky="w")
 Label(f, text="영화제목", width=8, height=1, padx=30, pady=10, bg="gray").grid(row=2, column=0, sticky="w")
 Label(f, text="음악제목", width=8, height=1, padx=30, pady=10, bg="gray").grid(row=3, column=0, sticky="w")
 Label(f, text="소리조절", width=8, height=1, bg="gray").grid(row=4, column=0)
